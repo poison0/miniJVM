ef70e9310ae4d03cec60f93666550b7b2cff3489
niu <1570012077@qq.com>
2024-01-02 18:02:05 +0800

    添加readClassFile方法

--- src/main/java/org/example/constant/ClassFieldTypeEnum.java
+++ src/main/java/org/example/constant/ClassFieldTypeEnum.java
@@ -9,13 +9,15 @@ import org.example.classfile.ClassFieldType;
  */
 @Getter
 public enum ClassFieldTypeEnum {
-    U1(ClassFieldType.U1.class),
-    U2(ClassFieldType.U2.class),
-    U4(ClassFieldType.U4.class),
-    U8(ClassFieldType.U8.class);
+    U1(ClassFieldType.U1.class,1),
+    U2(ClassFieldType.U2.class,2),
+    U4(ClassFieldType.U4.class,4),
+    U8(ClassFieldType.U8.class,8);
     private final Class<? extends ClassFieldType> clazz;
+    private final Integer length;
 
-    ClassFieldTypeEnum(Class<? extends ClassFieldType> clazz) {
+    ClassFieldTypeEnum(Class<? extends ClassFieldType> clazz, Integer length) {
         this.clazz = clazz;
+        this.length = length;
     }
 }
